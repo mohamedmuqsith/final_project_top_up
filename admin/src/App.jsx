411339db6a1492cb329018e57e3e46465208737a
Mukshith <mohamedmukshidh35@gmail.com>
2026-03-10 17:44:11 +0530

    admin dashboard setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import { useAuth } from "@clerk/clerk-react";
+import DashboardPage from "./pages/DashboardPage";
+import ProductsPage from "./pages/ProductsPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import OrdersPage from "./pages/OrdersPage";
+import CustomersPage from "./pages/CustomersPage";
+import PageLoader from "./components/PageLoader";
 
 function App() {
+  const { isSignedIn,isLoaded } = useAuth();
+
+  if(!isLoaded) return <PageLoader />; // to prevent the app from rendering before the auth state is loaded, which can cause a flash of the login page for a split second, even if the user is already signed in
+  
   return (
-    <div>
-      <h1>Home Page</h1>
-        <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-    </div>
-  )
+    <Routes>
+      <Route
+        path="/login"
+        element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />}
+      />
+
+      <Route path="/"
+        element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}
+      >
+        <Route index element={<Navigate to={"dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
