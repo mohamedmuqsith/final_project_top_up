8aabcff42aa2309a38c153621f3eb28ea4fa9e60
Mukshith <mohamedmukshidh35@gmail.com>
2026-03-10 23:49:06 +0530

    tiny fixes in login page

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,12 +1,10 @@
-import { SignIn } from '@clerk/clerk-react'
-import React from 'react'
+import { SignIn } from "@clerk/clerk-react";
 
 function LoginPage() {
   return (
-    <div>LoginPage
-        <SignIn />
+    <div className="h-screen hero">
+      <SignIn />
     </div>
-  )
+  );
 }
-
-export default LoginPage
+export default LoginPage;
