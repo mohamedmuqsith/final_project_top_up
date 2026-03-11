9c21acf5b0ae81c8acff630f6bbfb2f2226f8950
Mukshith <mohamedmukshidh35@gmail.com>
2026-03-11 20:56:53 +0530

    mobile app setup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,24 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import "../global.css"
+import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+const queryClient= new QueryClient();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+     <ClerkProvider  publishableKey={publishableKey} tokenCache={tokenCache}>
+    <QueryClientProvider client={queryClient}>
+       <Stack screenOptions={{headerShown:false}}/>
+    </QueryClientProvider>
+    </ClerkProvider>
+  )
 }
