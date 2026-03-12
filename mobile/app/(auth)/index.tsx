d43854e6d301ea38286fbd0c46aafe19aa586ecb
Mukshith <mohamedmukshidh35@gmail.com>
2026-03-12 16:38:11 +0530

    auth implemented mobile folder

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import useSocialAuth from "@/hooks/useSocialAuth";
+import {View,Text,Image,TouchableOpacity,ActivityIndicator,Platform,} from "react-native";
 
 const AuthScreen = () => {
-  return (
-    <View>
-      <Text>Auth Screen</Text>
-    </View>
-  )
-}
+  const { isLoading, handleSocialAuth } = useSocialAuth();
 
-export default AuthScreen
+  return (
+    <View className="flex-1 justify-center items-center bg-white px-8">
+      {/* DEMO IMAGE */}
+      <Image
+        source={require("../../assets/images/auth-image.png")}
+       className="size-96"
+        resizeMode="contain"
+      />
+
+      <View className="w-full gap-2 mt-3">
+        {/* GOOGLE SIGN IN BTN */}
+        <TouchableOpacity
+          className="flex-row items-center justify-center bg-white border border-gray-300 rounded-full px-6 py-2"
+          onPress={()=>handleSocialAuth("oauth_google")}
+          disabled={isLoading}
+          style={{
+            shadowOffset:{width:0,height:1},
+            shadowOpacity:0.1,
+            elevation:2, //This is for Android shadow
+          }}
+        >
+            {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"}/>
+           ):(
+            <View className="flex-row items-center justify-center">
+              <Image 
+                source={require("../../assets/images/google.png")}
+                className="size-10 mr-2"
+                resizeMode="contain"
+              />
+              <Text className="text-gray-700 font-medium">Continue with Google</Text>
+            </View>
+           )}
+        </TouchableOpacity>
+
+        {/* APPLE SIGN IN BTN (ONLY FOR IOS) */}
+                <TouchableOpacity
+          className="flex-row items-center justify-center bg-white border border-gray-300 rounded-full px-6 py-3"
+          onPress={()=>handleSocialAuth("oauth_apple")}
+          disabled={isLoading}
+          style={{
+            shadowOffset:{width:0,height:1},
+            shadowOpacity:0.1,
+            elevation:2, //This is for Android shadow
+          }}
+        >
+            {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"}/>
+           ):(
+            <View className="flex-row items-center justify-center">
+              <Image 
+                source={require("../../assets/images/apple.png")}
+                className="size-8 mr-2"
+                resizeMode="contain"
+              />
+              <Text className="text-gray-700 font-medium">Continue with Apple</Text>
+            </View>
+           )}
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center text-gray-500 text-xs leading-4 mt-6 px-2">
+        By signing up, you agree to our <Text className="text-blue-500">Terms</Text>
+        {", "}
+        <Text className="text-blue-500">Privacy Policy</Text>
+        {", and "}
+        <Text className="text-blue-500">Cookie Use</Text>
+      </Text>
+    </View>
+  );
+};
+
+export default AuthScreen;
